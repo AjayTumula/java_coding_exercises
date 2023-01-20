dafe6351d61d4ea6dd56760b0604652e93d81520
AjayTumula <ajaytumula72@gmail.com>
2023-01-20 19:35:51 +0000

    changes made to ex004

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -1,19 +1,24 @@
 package com.techreturners.exercise004;
 
+import java.sql.SQLOutput;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Exercise004 {
 
+    static final long GIGASECOND = (long)Math.pow(10,9);
+    private LocalDateTime dateTime;
+
     public Exercise004(LocalDate date) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.dateTime = date.atStartOfDay();
     }
 
     public Exercise004(LocalDateTime dateTime) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.dateTime = dateTime;
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return this.dateTime.plusSeconds(GIGASECOND);
     }
 }
