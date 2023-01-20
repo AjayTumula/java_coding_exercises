f469ac6baca7ec9c2078ca79bb333fe9efc9038c
AjayTumula <ajaytumula72@gmail.com>
2023-01-20 19:30:17 +0000

    made changes to ex003

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -3,7 +3,7 @@ package com.techreturners.exercise003;
 public class Exercise003 {
 
     int getIceCreamCode(String iceCreamFlavour) {
-        int icecreamcode = 0;
+        int icecreamcode = -1;
         for (int i = 0; i <= iceCreamFlavours().length - 1; i++) {
             if (iceCreamFlavours()[i].equals(iceCreamFlavour)) {
                 icecreamcode = i;
