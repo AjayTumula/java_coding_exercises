5945e79b1a30d39036f1eec2fa75a7b0aaa47b19
AjayTumula <ajaytumula72@gmail.com>
2023-01-20 19:38:19 +0000

    changes made to ex005 and ex005test

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,24 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
-    }
+        boolean[] alphabetList = new boolean[26];
+        int index = 0;
 
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) >= 'a' && input.charAt(i) <= 'z') {
+                index = input.charAt(i) - 'a';
+            } else if (input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') {
+                index = input.charAt(i) - 'A';
+            }
+            alphabetList[index] = true;
+        }
+
+        for (int j = 0; j <= 25; j++) {
+            if (alphabetList[j] == false) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
